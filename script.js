d371723ef716057e80afeb94529d118a68f315da
Justin Thomas <justin.thomas2@ontariotechu.net>
2021-06-09 15:35:41 -0400

    added preloader

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+// loader 
+const loader = document.querySelector(".loader")
+const site = document.querySelector(".site")
+
+function init() {
+
+    setTimeout(() => {
+        loader.style.opacity = 0
+        loader.style.display = "none";
+        site.style.display = "block";
+    }, 1300)  
+}   
+init()
+
+// nav
 const selectElement= (element) => document.querySelector(element);
-window.addEventListener('resize', () => {
+window.addEventListener("resize", () => {
     selectElement("header").classList.remove("active")
 })
 selectElement(".mobile-menu").addEventListener("click", () => {
